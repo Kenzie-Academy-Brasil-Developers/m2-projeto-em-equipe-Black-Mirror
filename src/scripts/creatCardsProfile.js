650ec0f9cfc8a0a5bdac52139d011df089806a99
GabrielRf <ti.gabrielrf@gmail.com>
2022-11-07 20:28:11 -0300

    styles: card list profile, script:creat card by DOM

---
+++ src/scripts/creatCardsProfile.js
@@ -0,0 +1,38 @@
+function creatCards(name, species, adopter, img){
+    const ulList = document.querySelector(".ul-petsList-profile")
+
+    let liList = document.createElement("li")
+    liList.classList.add("li-petsList-profile", "flex", "align-center")
+
+    let imgList = document.createElement("img")
+    imgList.classList.add("img-petsList")
+    imgList.src = `${img}`
+
+    let divDescription = document.createElement("div")
+    divDescription.classList.add("box-description-petsList", "flex", "column")
+
+    let spanName = document.createElement("span")
+    spanName.classList.add("span-description-petsList")
+    spanName.innerHTML = `<strong>Nome:</strong> ${name}`
+
+    let spanSpecies = document.createElement("span")
+    spanSpecies.classList.add("span-description-petsList")
+    spanSpecies.innerHTML = `<strong>Espécie:</strong> ${species}`
+
+    let spanAdopter = document.createElement("span")
+    spanAdopter.classList.add("span-description-petsList")
+    spanAdopter.innerHTML = `<strong>Adotável:</strong> ${adopter}`
+
+    let buttonUpdate = document.createElement("button")
+    buttonUpdate.classList.add("button-brand")
+    buttonUpdate.id = "button-brand"
+    buttonUpdate.innerHTML = "Atualizar"
+
+    divDescription.append(spanName, spanSpecies, spanAdopter, buttonUpdate)
+    liList.append(imgList, divDescription)
+    ulList.append(liList)
+}
+
+export {
+    creatCards
+}

2c6232fb7862c5bd19099351c54aa3ca7fd76d01
Doin <yuridoin@gmail.com>
2022-11-08 09:52:30 -0300

    feat: header/footer

---
+++ src/pages/home/index.js
@@ -0,0 +1,4 @@
+const menuIcon = document.querySelector('#menuIcon')
+const headerBtnsMobile = document.querySelector('#headerBtnsMobile')
+
+    menuIcon.addEventListener('click', ()=> headerBtnsMobile.classList.toggle('hide'))
